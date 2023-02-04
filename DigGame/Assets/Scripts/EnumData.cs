e9c5383b55d1bf8fc5bdcedfdf61393f8142566a
DESKTOP-K5JQ383\최준호 <9507ym@naver.com>
2023-02-04 12:05:55 +0900

    두더지 이동

---
+++ DigGame/Assets/Scripts/EnumData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TileType
+{
+    Null = 0,
+    Sand = 50,
+    Ground = 100,
+    DarkGound = 200,
+
+
+}
+
--- DigGame/Assets/Tile/CreateTileObjs.cs
+++ DigGame/Assets/Tile/CreateTileObjs.cs
@@ -16,6 +16,9 @@ public class CreateTileObjs : FieldObjectSingleton<CreateTileObjs>
     [SerializeField]
     private int h;
 
+    private Dictionary<Vector2Int, TileType> tileDatas = new Dictionary<Vector2Int, TileType>();
+    private Dictionary<Vector2Int, int> tileLifes = new Dictionary<Vector2Int, int>();
+
     private void Start()
     {
         CreateMap();
@@ -29,25 +32,57 @@ public class CreateTileObjs : FieldObjectSingleton<CreateTileObjs>
             for(int x = -w; x <= w; x++)
             {
                 TileBase tileBase = tileSand;
+                int ax = x;
+                int ay = -y;
                 if(y < h*0.3f)
+                {
                     tileBase = tileSand;
+                    tileDatas[new Vector2Int(ax, ay)] = TileType.Sand;
+                    tileLifes[new Vector2Int(ax, ay)] = 1;
+                }
                 else if(y < h * 0.7f)
+                {
                     tileBase = tileDirt0;
+                    tileDatas[new Vector2Int(ax, ay)] = TileType.Ground;
+                    tileLifes[new Vector2Int(ax, ay)] = 4;
+                }
                 else
+                {
                     tileBase = tileDirt1;
+                    tileDatas[new Vector2Int(ax, ay)] = TileType.DarkGound;
+                    tileLifes[new Vector2Int(ax, ay)] = 16;
+                }
 
-                tilemap.SetTile(new Vector3Int(x, -y, 0), tileBase);
+                tilemap.SetTile(new Vector3Int(ax, ay, 0), tileBase);
             }
         }
     }
 
-    public bool IsTile(int x,int y)
+    public TileType GetTile(int x,int y)
+    {
+        if (tileDatas.ContainsKey(new Vector2Int(x, y)))
+            return tileDatas[new Vector2Int(x, y)];
+        return TileType.Null;
+    }
+
+    public bool IsBlock(int x,int y)
     {
         return tilemap.GetTile(new Vector3Int(x, y, 0)) != null;
     }
 
-    public void RemoveTile(int x,int y)
+    public void DigTile(int x,int y)
     {
-        tilemap.SetTile(new Vector3Int(x, y, 0), null);
+        Vector2Int pos = new Vector2Int(x, y);
+        if (tileLifes.ContainsKey(pos) == false)
+            return;
+
+        if (tileLifes[pos] > 0)
+            tileLifes[pos]--;
+
+        if (tileLifes[pos] == 0)
+        {
+            tilemap.SetTile(new Vector3Int(x, y, 0), null);
+            tileDatas[pos] = TileType.Null;
+        }
     }
 }

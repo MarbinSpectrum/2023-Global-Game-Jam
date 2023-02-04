7b1fc4dd4f873c48febab5d3b65ccd58a279e080
DESKTOP-K5JQ383\최준호 <9507ym@naver.com>
2023-02-04 21:02:05 +0900

    경계선 설정

--- DigGame/Assets/Tile/TileOutline.cs
+++ DigGame/Assets/Tile/TileOutline.cs
@@ -5,17 +5,14 @@ using UnityEngine.Tilemaps;
 
 public class TileOutline : FieldObjectSingleton<TileOutline>
 {
-    //임시 객체
     [SerializeField]
     private Tilemap tilemap;
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// : x,y 위치에 pTileBase타일을 배치한다.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void SetTile(int x, int y, TileBase pTileBase)
     {
         tilemap.SetTile(new Vector3Int(x, y, 0), pTileBase);
-    }
-
-    public void ClearTile()
-    {
-        tilemap.ClearAllTiles();
     }
 }

2995b7de4f9ef737bb5b45ee923d3a4d259b1e5c
SSUHYUNKIM <yellosuhyun@naver.com>
2023-02-05 07:47:37 +0900

    #방 관련

---
+++ DigGame/Assets/Test/AnimalManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalManager : MonoBehaviour
+{
+
+    private Vector2Int _mousePosition;
+    private Vector2 _originalPosition;
+    private bool _crashed, _clicked;
+    public int ID;
+    private List<Vector2Int> _vectorList = new();
+
+    private void Awake()
+    {
+        _originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var mousePosition = GetMousePos();
+        _mousePosition = GetMousePos();
+
+        if (_clicked)
+        {
+            transform.position = mousePosition + new Vector2(0.5f, 0);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _clicked = !_clicked;
+        CheckNullTile();
+        if (!_clicked && _crashed)
+        {
+            transform.position = _originalPosition;
+            _crashed = false;
+        }
+        else if (!_crashed && !_clicked)
+        {
+            HouseManager.AddHouse(_vectorList);
+            this.GetComponent<PolygonCollider2D>().enabled = false;
+            transform.GetChild(0).gameObject.SetActive(true);
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    private void CheckNullTile()
+    {
+        TileManager tileManager = TileManager.instance;
+        _vectorList = AnimalData.CheckAnimalNumber(_mousePosition, ID);
+
+        foreach (Vector2Int movePos in _vectorList)
+        {
+            bool isBlock = tileManager.IsBlock(movePos.x, movePos.y);
+            if (isBlock || HouseManager.CheckHouse(movePos))
+            {
+                _crashed = true;
+                break;
+            }
+            _crashed = false;
+        }
+    }
+
+    private void CheckHouse()
+    {
+
+    }
+
+    Vector2Int GetMousePos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2Int pos = new Vector2Int(Mathf.FloorToInt(mousePos.x), Mathf.FloorToInt(mousePos.y));
+        return pos;
+    }
+}
